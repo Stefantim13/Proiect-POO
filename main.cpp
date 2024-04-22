06354a218aff51dda7b545f42311ac26f33d388c
Stefantim13 <timisescu.stef@gmail.com>
2024-04-22 16:57:45 +0300

    modif

--- main.cpp
+++ main.cpp
@@ -1,136 +1,224 @@
 #include <iostream>
-#include <array>
-#include <chrono>
-#include <thread>
-
+#include <vector>
+#include <random>
 #include <SFML/Graphics.hpp>
+#include <compare>
+class Camera
+{
+    sf::View view;
+    double view_speed = 0.1;
+    double currentY;
 
-#include <Helper.h>
-
-//////////////////////////////////////////////////////////////////////
-/// NOTE: this include is needed for environment-specific fixes     //
-/// You can remove this include and the call from main              //
-/// if you have tested on all environments, and it works without it //
-#include "env_fixes.h"                                              //
-//////////////////////////////////////////////////////////////////////
-
-
-//////////////////////////////////////////////////////////////////////
-/// This class is used to test that the memory leak checks work as expected even when using a GUI
-class SomeClass {
 public:
-    explicit SomeClass(int) {}
+    Camera(const sf::Vector2u &windowSize)
+    {
+        view.setSize(static_cast<double>(windowSize.x), static_cast<double>(windowSize.y));
+        view.setCenter(view.getSize() / 2.0f);
+    }
+    
+    void update(const sf::Vector2f &playerPosition)
+    {
+        currentY = std::min(playerPosition.y, view.getCenter().y);
+        
+        view.setCenter(view.getCenter().x, currentY);
+    }
+    const sf::View &getView() const
+    {
+        return view;
+    }
 };
 
-SomeClass *getC() {
-    return new SomeClass{2};
-}
-//////////////////////////////////////////////////////////////////////
+class Platform
+{
+    const double length = 100, width = 20;
+    int type; /// tip de platforma (rezista la infinit, se distruge dupa ce sari o data pe ea, te propulseaza mai mult in sus...)
+    double x, y;
+    sf::RectangleShape plat;
 
-
-int main() {
-    ////////////////////////////////////////////////////////////////////////
-    /// NOTE: this function call is needed for environment-specific fixes //
-    init_threads();                                                       //
-    ////////////////////////////////////////////////////////////////////////
-    ///
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
+public:
+    Platform(double x_, double y_, int type_ = 0) : x(x_), y(y_), type(type_)
+    {
+        plat.setSize(sf::Vector2f(length, width));
+        plat.setPosition(x, y);
+        plat.setFillColor(sf::Color::Green);
     }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
+
+    void draw(sf::RenderWindow &window)
+    {
+        window.draw(plat);
     }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
 
-    SomeClass *c = getC();
-    std::cout << c << "\n";
-    delete c;
+    double getX()
+    {
+        return x;
+    }
 
-    sf::RenderWindow window;
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
-    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
-    /// This is needed so we do not burn the GPU                            ///
-    window.setVerticalSyncEnabled(true);                            ///
-    /// window.setFramerateLimit(60);                                       ///
-    ///////////////////////////////////////////////////////////////////////////
+    double getY()
+    {
+        return y;
+    }
 
-    while(window.isOpen()) {
-        bool shouldExit = false;
-        sf::Event e{};
-        while(window.pollEvent(e)) {
-            switch(e.type) {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::Resized:
-                std::cout << "New width: " << window.getSize().x << '\n'
-                          << "New height: " << window.getSize().y << '\n';
-                break;
-            case sf::Event::KeyPressed:
-                std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
-                if(e.key.code == sf::Keyboard::Escape)
-                    shouldExit = true;
-                break;
-            default:
+    double getLength()
+    {
+        return length;
+    }
+
+    double getWidth()
+    {
+        return width;
+    }
+
+    sf::FloatRect getBounds() const
+    {
+        return plat.getGlobalBounds();
+    }
+};
+
+class Doodle
+{
+    const double dimension_x = 50, dimension_y = 100;
+    const double gravity = 0.5;
+    const double jumpStrength = -16;
+    double jump = 0;
+    double x = 275, y = 800;
+    sf::RectangleShape rect;
+
+public:
+    Doodle()
+    {
+        rect.setSize(sf::Vector2f(dimension_x, dimension_y));
+        rect.setFillColor(sf::Color::Red);
+        rect.setPosition(x, y);
+    }
+
+    void draw(sf::RenderWindow &window)
+    {
+        window.draw(rect);
+    }
+
+    void update(const std::vector<Platform> &platforms)
+    {
+        jump += gravity;
+        y += jump;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            x += 15;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            x -= 15;
+        }
+
+        for (const auto &platform : platforms)
+        {
+            if (checkCollision(platform))
+            {
+                jump = jumpStrength;
                 break;
             }
         }
-        if(shouldExit) {
-            window.close();
-            break;
-        }
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(300ms);
 
-        window.clear();
-        window.display();
+        rect.setPosition(x, y);
     }
+
+    bool checkCollision(const Platform &platform)
+    {
+        sf::FloatRect doodleBounds, platformBounds;
+        doodleBounds = rect.getGlobalBounds();
+        platformBounds = platform.getBounds();
+
+        return jump > 0 && doodleBounds.intersects(platformBounds);
+    }
+
+    double getX()
+    {
+        return x;
+    }
+
+    double getY()
+    {
+        return y;
+    }
+    
+};
+
+class Game
+{
+    Doodle doodle;
+    Camera camera;
+    std::vector<Platform> platforms;
+    sf::RenderWindow &window;
+    const int verticalSpacing = 200;
+    const int platformCount = 100;
+    double x, y;
+
+public:
+    Game(sf::RenderWindow &window_) : window(window_), camera(window_.getSize())
+    {
+        generateInitialPlatforms();
+    }
+    void generateInitialPlatforms()
+    {
+        platforms.emplace_back(250, 950);
+
+        for (int i = 1; i < platformCount; ++i)
+        {
+            x = randomX();
+            y = platforms[platforms.size()-1].getY() - verticalSpacing;
+            platforms.emplace_back(x, y);
+        }
+    }
+    double randomX()
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dis(0, window.getSize().x - 100);
+        return dis(gen);
+    }
+
+    void drawPlatforms()
+    {
+        for (auto &platform : platforms)
+        {
+            platform.draw(window);
+        }
+    }
+
+    std::vector<Platform> &getPlatforms()
+    {
+        return platforms;
+    }
+
+    void run()
+    {
+        while (window.isOpen())
+        {
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            doodle.update(platforms);
+            camera.update(sf::Vector2f(doodle.getX(), doodle.getY()));
+            window.clear();
+            window.setView(camera.getView());
+            drawPlatforms();
+            doodle.draw(window);
+            window.display();
+        }
+    }
+
+};
+
+int main()
+{
+    sf::RenderWindow window;
+    window.create(sf::VideoMode({800, 1200}), "Doodle Jump", sf::Style::Default);
+    window.setVerticalSyncEnabled(true); 
+    Game game(window);
+    game.run();
     return 0;
 }
